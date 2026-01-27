acc1efc28cb3db4aaf760eaaefaa96dbcd548946
Adbrskk <aedobroskok@gmail.com>
2026-01-27 19:54:50 +0100

    Add React homework 1 properly as normal folder

---
+++ react-hw-1/src/components/ImageComponent/ImageComponent.jsx
@@ -0,0 +1,13 @@
+function ImageComponent() {
+  return (
+    <div>
+      <h2>Image</h2>
+      <img
+        src="https://via.placeholder.com/300"
+        alt="example"
+      />
+    </div>
+  );
+}
+
+export default ImageComponent;
